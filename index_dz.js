957a58d5fe0ee7b01abfba336883d8acb3c168fc
Роман Хоружий <roman.khoruzhy@gmail.com>
2024-10-29 22:52:41 +0300

    lesson 1 dz

---
+++ index_dz.js
@@ -0,0 +1,38 @@
+setImmediate(() => {
+  console.log('Был диван,');
+});
+
+setTimeout(() => {
+  console.log('Выйди вон!');
+}, 100);
+
+setImmediate(() => {
+  console.log('На диване');
+});
+
+process.nextTick(() => {
+  console.log('Чемодан,');
+});
+
+setTimeout(() => {
+  console.log('Кто не верит –');
+}, 10);
+
+setImmediate(() => {
+  console.log('Ехал слон.');
+});
+
+process.nextTick(() => {
+  console.log('В чемодане');
+});
+
+console.log('Плыл по морю');
+
+// Плыл по морю
+// Чемодан,
+// В чемодане
+// Был диван,
+// На диване
+// Ехал слон.
+// Кто не верит –
+// Выйди вон!
